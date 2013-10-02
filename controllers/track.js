60af0cf8cca8b1d68f6af254f8dfa3233f909818
bfisher <brent.fisher@careerbuilder.com>
2013-10-02 16:10:03 -0400

    save a few more things about a request to the db

--- controllers/track.js
+++ controllers/track.js
@@ -1,9 +1,23 @@
 var trackingProvider = new TrackingProvider();
 
 exports.tracking = function(req, res){
-  var data = req.query;
+  //will break this out into a model soon
+  var data = 
+  {
+    qsparams: req.query,
+    ip: req.ip,
+    url: req.url,
+    xhr: req.xhr
+  };
+
   console.log(data);
-  trackingProvider.save(req.query, function(e,d) {
+  //console.log(req.cookies);
+  //console.log(req.route);
+  console.log(req.ip);
+  console.log(req.url);
+  console.log(req.xhr);
+
+  trackingProvider.save(data, function(e,d) {
     console.log(e);
   });
   res.send("tracked");
